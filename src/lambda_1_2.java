0d9e42c3e9bd62c685cba6c355bd87b9ebaa9bd7
Dmytro_Adonin <dmytro_adonin@epam.com>
2015-11-10 12:14:30 +0200

    Lambda for output files in root path with given extension

--- src/lambda_1_2.java
+++ src/lambda_1_2.java
@@ -4,11 +4,12 @@ import java.util.Arrays;
 /**
  * Created by Dmytro_Adonin on 11/9/2015.
  */
-public class lambda_1_1 {
+public class lambda_1_2 {
 
     public static void main(String[] args) {
         String path = "C:\\"; //should be replaced with arg
         File root = new File(path);
+        //non-lambda style
         /*File[] dirs = root.listFiles(new FileFilter() {
             @Override
             public boolean accept(File pathname) {
---
+++ src/lambda_1_3.java
@@ -0,0 +1,37 @@
+import java.io.File;
+import java.util.Arrays;
+
+/**
+ * @author Dmitry Adonin
+ * @since 11/10/2015
+ */
+public class lambda_1_3 {
+    public static void main(String[] args) {
+
+        String rootPath = "C:\\Windows";
+        String ext = ".exe";
+
+        File root = new File(rootPath);
+        //non-lambda style
+        /*File[] files = root.listFiles(new FileFilter() {
+            @Override
+            public boolean accept(File pathname) {
+                if (!pathname.isDirectory()) {
+                    String filename = pathname.getName();
+                    int index = filename.lastIndexOf('.');
+                    return filename.substring(index).equals(ext);
+                }
+                return false;
+            }
+        });*/
+        File[] files = root.listFiles((pathname) -> {
+            if (!pathname.isDirectory()) {
+                String filename = pathname.getName();
+                int index = filename.lastIndexOf('.');
+                return filename.substring(index).equals(ext);
+            }
+            return false;
+        });
+        Arrays.asList(files).forEach(System.out::println);
+    }
+}
