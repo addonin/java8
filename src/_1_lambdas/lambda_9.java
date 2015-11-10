95e11f3c2d211009e77db707ecaded8a6c364bbc
Dmytro_Adonin <dmytro_adonin@epam.com>
2015-11-10 13:44:49 +0200

    Lambda for aggregating two Runnable

---
+++ src/_1_lambdas/lambda_9.java
@@ -0,0 +1,22 @@
+package _1_lambdas;
+
+/**
+ * @author Dmitry Adonin
+ * @since 11/10/2015
+ */
+public class lambda_9 {
+    public static void main(String[] args) {
+        new Thread(andThen(() -> {
+            System.out.println("I'm R1");
+        }, () -> {
+            System.out.println("I'm R2");
+        })).start();
+    }
+
+    public static Runnable andThen(Runnable r1, Runnable r2) {
+        return () -> {
+            r1.run();
+            r2.run();
+        };
+    }
+}
