8a8dff0bdcdd6bac3949686643422895475cb039
Dmytro_Adonin <dmytro_adonin@epam.com>
2015-11-11 13:39:05 +0200

    IntStream().range(), mapToObj()

---
+++ src/_2_iostreams/IOStreams_6.java
@@ -0,0 +1,19 @@
+package _2_iostreams;
+
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+/**
+ * @author Dmitry Adonin
+ * @since 11/11/2015
+ */
+public class IOStreams_6 {
+    public static void main(String[] args) {
+        Stream<Character> stream = characterStream("Hello");
+        stream.forEach(System.out::println);
+    }
+
+    public static Stream<Character> characterStream(String s) {
+       return IntStream.range(0, s.length()).mapToObj(s::charAt);
+    }
+}
