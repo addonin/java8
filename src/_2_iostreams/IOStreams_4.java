7e4add9541d0df01899e619f7bdad13fb311e773
Dmytro_Adonin <dmytro_adonin@epam.com>
2015-11-11 12:28:42 +0200

    Wrappers stream VS primitives stream

---
+++ src/_2_iostreams/IOStreams_4.java
@@ -0,0 +1,16 @@
+package _2_iostreams;
+
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+/**
+ * @author Dmitry Adonin
+ * @since 11/11/2015
+ */
+public class IOStreams_4 {
+    public static void main(String[] args) {
+        int[] values = {1,2,3,4,5};
+        Stream objStream = Stream.of(values); // stream of objects
+        IntStream  intStream = IntStream.of(values); // stream of int's, fits better for primitives
+    }
+}
