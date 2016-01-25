697ed97935e5e3558dd74a39ecca7bfe7c22b559
Dmytro_Adonin <Dmytro_Adonin@epam.com>
2016-01-25 11:17:44 +0200

    Change brightness of image with factor

--- src/_3_examples/Example_8.java
+++ src/_3_examples/Example_8.java
@@ -9,37 +9,26 @@ import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
-import java.util.function.UnaryOperator;
-
 /**
- * @author dimon
- * @since 01/12/15.
+ * Created by dmytro_adonin on 1/22/2016.
  */
-public class Example_5 extends Application {
+public class Example_8 extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
         Image image = new Image("http://i68.tinypic.com/o54brd.jpg");
         //Image newImage = transform(image, Color::brighter);
-        Image newImage = transform(image, (x, y, c) -> (
-            (x <= 10 || x >= image.getWidth() - 10 || y <= 10 || y >= image.getHeight() - 10)
-                    ? Color.GREY : c)
-        );
+        Image newImage = transform(image, getColorTransformerWithBorder(image, 25, Color.WHITE));
         stage.setScene(new Scene(new HBox(new ImageView(image), new ImageView(newImage))));
         stage.show();
     }
 
-    /*public static Image transform(Image in, UnaryOperator<Color> f) {
-        int x = (int) in.getWidth();
-        int y = (int) in.getHeight();
-        WritableImage out = new WritableImage(x, y);
-        for (int i = 0; i < x; i++) {
-            for (int j = 0; j < y; j++) {
-                out.getPixelWriter().setColor(i, j, f.apply(in.getPixelReader().getColor(i, j)));
-            }
-        }
-        return out;
-    }*/
+    private static ColorTransformer getColorTransformerWithBorder(Image image, int px, Color color) {
+        return (x, y, colorAtXY) -> (
+            (x <= px || x >= image.getWidth() - px || y <= px || y >= image.getHeight() - px)
+                    ? color : colorAtXY
+        );
+    }
 
     public static Image transform(Image in, ColorTransformer transformer) {
         int width = (int) in.getWidth();
@@ -59,5 +48,3 @@ public class Example_5 extends Application {
     }
 
 }
-
-
